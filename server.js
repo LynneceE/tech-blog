1c9e8b88e1dc341468c747c3c7436d63fbf56479
bhari eutsey <bharilynn@outlook.com>
2022-02-21 22:39:12 -0600

    switch to mvc, create working homepage with handlebars

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controller');
 const sequelize = require('./config/connection');
 
 
@@ -9,10 +9,17 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const exphbs = require('express-handlebars');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => { // change force: false to true when making changes to Sequelize models
-  app.listen(PORT, () => console.log('Now listening', `http:localhost:${PORT}`));
+  app.listen(PORT, () => console.log('Now listening', `http://localhost:${PORT}`));
 }); 
