fbe5c367d14dbadbf510fa9390ab92a2bc2c62b2
bhari eutsey <bharilynn@outlook.com>
2022-02-21 21:27:55 -0600

    post routes working

--- models/index.js
+++ models/index.js
@@ -2,4 +2,16 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// ASSOCIATIONS
+
+//nba has many teams
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//this team belongs to the nba
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post }; 
